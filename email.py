c04916f073b465b8aa6fdc1859ca3239453ef572
kushalkp88 <47237223+kushalkp88@users.noreply.github.com>
2019-03-11 10:33:17 +0530

    Add files via upload

--- email.py
+++ email.py
@@ -1,5 +1,15 @@
-def fun():
-	print("hi")
-
-
-fun()
+#email
+import smtplib
+try:
+	s=smtplib.SMTP('smtp.gmail.com','587')
+	s.starttls()
+	sender='kushal.18bcs@gmail.com'
+	reciver='kushalkp88@gmail.com'
+	msg="hii"
+	s.login(sender,'password')
+	s.sendmail(sender,reciver,msg)
+except:
+	print("error")
+else:
+	print("success")
+	s.quit()
