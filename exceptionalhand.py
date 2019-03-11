72bdc1edc2dbf67aeb4685c6479a6f766e6dcca7
kushalkp88 <47237223+kushalkp88@users.noreply.github.com>
2019-03-11 09:16:21 +0530

    Add files via upload

---
+++ exceptionalhand.py
@@ -0,0 +1,53 @@
+#exceptional handeling
+try:
+	s=1
+	print(s)
+except:
+	print("error")
+else:
+	print("hii")
+finally:
+	print("sucess")
+
+#exceptional handeling
+try:
+	#r=1
+	print(r)
+except:
+	print("error")
+else:
+	print("hii")
+finally:
+	print("sucess")
+
+
+try:
+	#q=1
+	print(q)
+	raise NameError
+except NameError:
+	print("u have not defined s")
+except:
+	print("unscuss")
+else:
+	print("hii")
+
+finally:
+	print("sucess")
+
+
+'''
+try:
+	#g=1
+	print(g)
+	raise kusHal
+except kusHal:
+	print("u have not defined s")
+except:
+	print("unscuss")
+else:
+	print("hii")
+
+finally:
+	print("sucess")
+'''
